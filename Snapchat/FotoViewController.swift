47061867fa252741660cffcd9f0b4f07cbacf232
Rodrigo Abreu <rodrigoabreu@rodrigos-macbook-pro.local>
2017-12-02 15:06:26 -0200

    Selecionando fotos

--- Snapchat/FotoViewController.swift
+++ Snapchat/FotoViewController.swift
@@ -7,13 +7,43 @@
 //
 
 import UIKit
+import Photos
 
-class FotoViewController: UIViewController {
-
+class FotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+   
+    
+    @IBOutlet var imagem: UIImageView!
+    @IBOutlet var descricao: UITextField!
+    
+    let imagePicker = UIImagePickerController()
+    
+    @IBAction func selecionarFoto(_ sender: Any) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+                        
+            imagePicker.sourceType = .savedPhotosAlbum
+            present(self.imagePicker, animated: true, completion: nil)
+            //No info.plist é necessário solicitar permissão para o usuário utilizar a camera
+            
+        }
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let imagemRecuperada = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        imagem.image = imagemRecuperada        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {

0f0bd8bcb5d78c84909bafd8abf378cabb298d75
Rodrigo Abreu <rodrigoabreu@rodrigos-macbook-pro.local>
2017-12-07 18:14:25 -0200

    Projeto concluido

--- Snapchat/ViewControllers/CadastroViewController.swift
+++ Snapchat/ViewControllers/CadastroViewController.swift
@@ -128,6 +128,11 @@ class CadastroViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
